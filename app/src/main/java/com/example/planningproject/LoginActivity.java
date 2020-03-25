ed5de0d88b5302361f26d901e2a73a39f2fc4006
RemiRemaud <85800r@gmail.com>
2020-03-25 11:50:34 +0100

    Admin fonctionnel

--- app/src/main/java/com/example/planningproject/LoginActivity.java
+++ app/src/main/java/com/example/planningproject/LoginActivity.java
@@ -1,11 +1,14 @@
 package com.example.planningproject;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.os.Vibrator;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.firebase.ui.auth.data.model.User;
@@ -15,15 +18,26 @@ public class LoginActivity extends AppCompatActivity {
     public EditText accessCode;
     Button btnLogIn;
     Vibrator vib;
+    public TextView log;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+
         accessCode = findViewById(R.id.acessCode);
         btnLogIn = findViewById(R.id.btnLogIn);
+        log = findViewById(R.id.resultlogin);
         vib=(Vibrator)getSystemService(UserActivity.VIBRATOR_SERVICE);
+
+        Intent I = getIntent();
+        if (I.hasExtra("Resultat")) {
+           log.setText("Code d'accès incorrect !");
+           log.setTextColor(Color.parseColor("#ee1010"));
+        }
+
+
 
         btnLogIn.setOnClickListener(new View.OnClickListener() {
             @Override
